f35b1863cb26e1014717baaf8d6ff4a0731d9dfb
mustafa861 <mustafa1212adnan@gmail.com>
2025-06-25 16:55:10 +0500

    .

--- simple_agent.py
+++ simple_agent.py
@@ -1,7 +1,9 @@
 import os
-
-from agents import Agent, RunConfig, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
+from agents import Agent, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
+from agents.run import RunConfig
 from dotenv import load_dotenv, find_dotenv
+from agents.tool import function_tool
+import asyncio
 
 load_dotenv(find_dotenv())
 
@@ -9,11 +11,11 @@ gemini_api_key = os.getenv("GEMINI_API_KEY")
 
 provider = AsyncOpenAI(
     api_key=gemini_api_key,
-    base_url="https://openrouter.ai/api/v1"
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
 model = OpenAIChatCompletionsModel(
-    model="google/gemini-2.0-flash-exp:free",
+    model="gemini-2.0-flash",
     openai_client=provider
 )
 
@@ -23,11 +25,25 @@ run_config = RunConfig(
     tracing_disabled=True
 )
 
-agent = Agent(
+
+
+
+
+
+
+
+
+async def main():
+    agent = Agent(
     instructions="You are a helpful AI assistant that provides clear and accurate responses to user queries.",
-    name="Assistant"
+    name="Assistant",
 )
 
-result = Runner.run_sync(agent, "Hello, how are you?", run_config=run_config)
 
-print(result.final_output)
+    result = await Runner.run(agent, "sum of 2+3", run_config=run_config)
+    print(result.final_output)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
